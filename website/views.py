2ca259a59d7c8ef6a56e4e8beb8f876cbf66c554
Shannen Geller <shannengeller7@gmail.com>
2023-04-05 00:06:21 -0400

    routed landing page, need to fix other routing

--- website/views.py
+++ website/views.py
@@ -4,4 +4,16 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("BT-Intro.html")
+
+# @views.route('/EnterBday')
+# def enterBDay():
+#     return render_template("BT-EnterBDay.html")
+
+# @views.route('/SignIn')
+# def enterBDay():
+#     return render_template("BT-SignIn.html")
+
+# @views.route('/ViewBdays')
+# def enterBDay():
+#     return render_template("BT-ViewBDay.html")
