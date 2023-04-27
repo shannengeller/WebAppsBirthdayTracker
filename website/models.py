d6e20d2d10c892442811bfa7bda78ee7fbf7d2ea
Sepehr Saljooghi <ssaljooghi5@gmail.com>
2023-04-27 16:47:15 -0400

    added database, has errors for now

---
+++ website/models.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+
+
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect('birthdays.db')
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS birthdays
+                   (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, relationship TEXT, dob TEXT, gift_ideas TEXT)''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+
+@app.route('/')
+def index():
+    return render_template('BT-EnterBDay.html')
+
+@app.route('/add_birthday', methods=['POST'])
+def add_birthday():
+    first_name = request.form['inputFirstName']
+    last_name = request.form['inputLastName']
+    relationship = request.form['relationship']
+    dob = request.form['dob']
+    gift_ideas = request.form['giftIdeas']
+
+    conn = sqlite3.connect('birthdays.db')
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO birthdays (first_name, last_name, relationship, dob, gift_ideas) VALUES (?, ?, ?, ?, ?)''', (first_name, last_name, relationship, dob, gift_ideas))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
