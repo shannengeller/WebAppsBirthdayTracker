9ef2b4912f20f364d5b951f96965f488421dd7c9
AlexButtonECU <alexjbutton@gmail.com>
2023-04-29 12:12:26 -0400

    fixing db stuff

--- website/views.py
+++ website/views.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, Flask
+import sqlite3
 
 views = Blueprint('views', __name__)
 
+app = Flask(__name__)
+def init_db():
+    conn = sqlite3.connect('birthdays.db')
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS birthdays
+                   (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, relationship TEXT, dob TEXT, gift_ideas TEXT)''')
+    conn.commit()
+    conn.close()
+
+init_db()
 
 @views.route('/')
 def home():
@@ -12,8 +22,23 @@ def home():
 def intro():
     return render_template("BT-Intro.html")
 
-@views.route('/BT-EnterBDay.html', methods=['POST', 'GET'])
+@views.route('/BT-EnterBDay.html')
 def enterBDay():
+    return render_template("BT-EnterBDay.html")
+
+@views.route('/BT-EnterBDay.html', methods=['POST'])
+def enterBDay2():
+    first_name = request.form['inputFirstName']
+    last_name = request.form['inputLastName']
+    relationship = request.form['relationship']
+    dob = request.form['dob']
+    gift_ideas = request.form['giftIdeas']
+
+    conn = sqlite3.connect('birthdays.db')
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO birthdays (first_name, last_name, relationship, dob, gift_ideas) VALUES (?, ?, ?, ?, ?)''', (first_name, last_name, relationship, dob, gift_ideas))
+    conn.commit()
+    conn.close()
     return render_template("BT-EnterBDay.html")
 
 @views.route('/BT-SignIn.html')
